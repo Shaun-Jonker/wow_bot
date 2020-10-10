e2eb401839269ad53fcc842bca92dcd2e1629b32
Shaun-Jonker <shaun.c.jonker@gmail.com>
2020-10-10 09:05:55 +0200

    raid create now writes to a csv file incase the server shuts down

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import random
 import time
 import logging
 from discord.ext import commands
+import csv
+from writer import Writer
+
 
 # This is for me to log to a file if there are any errors that occur and to log bugs for fixing
 logger = logging.getLogger('discord')
@@ -12,6 +15,8 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 raid_list = list()
+delete_list = list()
+w = Writer()
 
 # Greetings List
 greetings = ["We will persevere!",
@@ -60,7 +65,7 @@ greetings = ["We will persevere!",
 
 # This is the basic discord bot setup
 client = discord.Client()
-bot = commands.Bot(command_prefix="$")
+bot = commands.Bot(command_prefix="!")
 
 
 # Confirmation that the bot has started and connected
@@ -108,43 +113,93 @@ async def roll(ctx, *args):
 
 # this is the creation of raid events that you can create a raid event to be viewed by members
 raid_list = []
+row_id = 0
 
 
 # raid creation command
 @bot.command()
-async def raid_create(ctx, raid_name, players, date, raid_time, description):
-    global raid_list
-    temp = [raid_name, players, date, raid_time, description]
-    raid_list.append(temp)
-    print(raid_list)
-    await ctx.send(f'{temp[0]} raid has been created for {temp[1]}, and will take place on {temp[2]} at {temp[3]}')
+async def raid_create(ctx, raid_name=None, players=None, date=None, raid_time=None, description=None):
+    if raid_name is None:
+        await ctx.send(
+            'To create a raid type "$raid_create \'Raid name\' \'Numer of players\' \'Date\' \'Time\' \'Desscription\'"')
+    else:
+        global raid_list
+        temp = [raid_name, players, date, raid_time, description]
+        raid_list.append(temp)
+        print("before", raid_list)
+        print(raid_list)
+        w.append(raid_list)
+        lines = []
+        with open("raid_list.csv", 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if not row:
+                    continue
+                lines.append(row)
+        with open('raid_list.csv', 'w', newline='') as writeFile:
+            writer = csv.writer(writeFile)
+            writer.writerows(lines)
 
+        print("after", raid_list)
+        await ctx.send(f'{temp[0]} raid has been created for {temp[1]}, and will take place on {temp[2]} at {temp[3]}')
+    raid_list = []
 
 # raid search command for regular members
 @bot.command()
 async def raid_events(ctx):
-    for raid in raid_list:
-        embeded = discord.Embed(
-            title=f'{raid[0]} Raid.',
-            description=raid[4],
-            colour=discord.Color.dark_orange()
-        )
-        embeded.set_footer(text="See you all there!")
-        embeded.set_image(url='https://cdn.discordapp.com/avatars/762972780727631873/cb3674efcc266269387c1365eee126e8'
-                              '.png?size=256')
-        embeded.set_thumbnail(url='https://cdn.discordapp.com/avatars/762972780727631873'
-                                  '/cb3674efcc266269387c1365eee126e8.png?size=256')
-        embeded.set_author(name="Axe of Grombrindal Guild Master", icon_url='https://cdn.discordapp.com/avatars'
-                                                                            '/762972780727631873'
-                                                                            '/cb3674efcc266269387c1365eee126e8.png'
-                                                                            '?size=256')
-        embeded.add_field(name='Where', value=raid[0], inline=False)
-        embeded.add_field(name='Players', value=raid[1], inline=True)
-        embeded.add_field(name="Date", value=raid[2], inline=True)
-        embeded.add_field(name="Time", value=raid[3], inline=True)
+    with open("raid_list.csv", 'r') as file:
+        reader = csv.reader(file)
 
-        await ctx.send(embed=embeded)
+        for raid in reader:
+            print(f"Reading {raid} from file!")
+            embeded = discord.Embed(
+                title=f'{raid[0]} Raid.',
+                description=raid[4],
+                colour=discord.Color.dark_orange()
+            )
+            embeded.set_footer(text="See you all there!")
+            embeded.set_image(url='https://cdn.discordapp.com/avatars/762972780727631873/cb3674efcc266269387c1365eee126e8'
+                                  '.png?size=256')
+            embeded.set_thumbnail(url='https://cdn.discordapp.com/avatars/762972780727631873'
+                                      '/cb3674efcc266269387c1365eee126e8.png?size=256')
+            embeded.set_author(name="Axe of Grombrindal Guild Master", icon_url='https://cdn.discordapp.com/avatars'
+                                                                                '/762972780727631873'
+                                                                                '/cb3674efcc266269387c1365eee126e8.png'
+                                                                                '?size=256')
+            embeded.add_field(name='Where', value=raid[0], inline=False)
+            embeded.add_field(name='Players', value=raid[1], inline=True)
+            embeded.add_field(name="Date", value=raid[2], inline=True)
+            embeded.add_field(name="Time", value=raid[3], inline=True)
+
+            await ctx.send(embed=embeded)
+
+@bot.command()
+async def raid_delete(ctx, name=None, date=None, raid_time=None):
+    if name is None:
+        await ctx.send("Please type !raid_delete 'raid name' 'date' 'time'.")
+    else:
+        lines = list()
+
+        with open('raid_list.csv', 'r') as readFile:
+
+            reader = csv.reader(readFile)
+
+            for row in reader:
+                lines.append(row)
+                if not row:
+                    continue
+                elif row[0] == name and row[2] == date and row[3] == raid_time:
+                    lines.remove(row)
+                    await ctx.send("Raid deleted")
+                # fix this statement due to repeat output
+                else:
+                    await ctx.send("Could not find that raid!")
+
+        with open('raid_list.csv', 'w', newline='') as writeFile:
+
+            writer = csv.writer(writeFile)
+
+            writer.writerows(lines)
 
 
-
-bot.run("NzYyOTcyNzgwNzI3NjMxODcz.X3w7xg.RwwDYmafeqSORjNEmKvw5uCf9Xo")
+bot.run("NzY0MTg0MzMyNjM3OTYyMjcx.X4CkHg.TwmWI3w21AUjo_0ern9aO8HBxCc")
